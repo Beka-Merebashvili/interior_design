6e7b9bcd68daf904d691a40c5e138ca00c673b04
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2023-09-29 14:37:46 +0400

    make hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
- 
+ import Hero from "./views/Hero"
 
 function App() {
    
 
   return (
     <>
-      <h1 className="text-2xl font-bold underline">
-      Hello world!
-    </h1>
+     <Hero />
     </>
   )
 }
