422bb5f0d2c3e83adde335fcb1d43142c4293310
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2023-10-08 21:33:44 +0400

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
  import Care from "./views/Care"
+import Footer from "./views/Footer"
 import Hero from "./views/Hero"
 import Products from "./views/Products"
 import Reference from "./views/Reference"
@@ -14,6 +15,7 @@ function App() {
      <Products />
      <Reference />
      <Care />
+     <Footer />
     </>
   )
 }
